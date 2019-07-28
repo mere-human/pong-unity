991a9b3b2372bc70f93e77b075165ac8f2f8bfd3
mere-human <invulnerable.reason@gmail.com>
2019-07-28 19:31:20 +0300

    Add horizontal movement for player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public KeyCode moveUp = KeyCode.UpArrow;
+    public KeyCode moveDown = KeyCode.DownArrow;
+
+    private Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velocityY = 0;
+        if (Input.GetKey(moveUp))
+        {
+            velocityY = speed;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            velocityY = -speed;
+        }
+        rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, velocityY);
+    }
+}
