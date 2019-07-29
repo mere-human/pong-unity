517498129084709c9426a9585defec4aa71b50c4
mere-human <invulnerable.reason@gmail.com>
2019-07-29 22:40:38 +0300

    Fix walls positioning

--- Assets/GameSetup.cs
+++ Assets/GameSetup.cs
@@ -18,20 +18,19 @@ public class GameSetup : MonoBehaviour
     void Start()
     {
         var wallThickness = 1f;
-        var wallXOffset = 1.5f;
-        var wallYOffset = 0.5f;
 
         topWall.size = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(Screen.width * 2.0f, 0f, 0f)).x, wallThickness);
-        topWall.offset = new Vector2(wallXOffset, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y + wallYOffset);
+        topWall.offset = new Vector2(0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y + 1.5f);
 
         bottomWall.size = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(Screen.width * 2.0f, 0f, 0f)).x, wallThickness);
-        bottomWall.offset = new Vector2(wallXOffset, mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y - wallYOffset * 2);
+        bottomWall.offset = new Vector2(0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y - 1.5f);
 
         leftWall.size = new Vector2(wallThickness, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2.0f, 0f)).y);
-        leftWall.offset = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f + 2.5f, 0f);
+        leftWall.offset = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
 
-        rightWall.size = new Vector2(1f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height*2, 0f)).y);
-        rightWall.offset = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 1.5f, 0f);
+        rightWall.size = new Vector2(wallThickness, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height*2, 0f)).y);
+        rightWall.offset = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
+
 
         player1.position = new Vector3(mainCamera.ScreenToWorldPoint(new Vector3(75f, 0)).x, 0, 0);
         player2.position = new Vector3(mainCamera.ScreenToWorldPoint(new Vector3(Screen.width - 75f, 0)).x, 0, 0);
