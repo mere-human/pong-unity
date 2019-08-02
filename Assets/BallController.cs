792559671a346d3bf68000b689675790595c5d5d
mere-human <invulnerable.reason@gmail.com>
2019-08-02 21:40:08 +0300

    Add ball reset button

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -12,13 +12,25 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
         Invoke(nameof(GoBall), 2);
     }
 
+    private void Update()
+    {
+        // bump up the velocity
+        var xVel = rigidbody2d.velocity.x;
+        if (Mathf.Abs(xVel) < 18 && !Mathf.Approximately(xVel, 0))
+        {
+            // actually did not get there, but...
+            xVel = (xVel > 0) ? 20 : -20;
+        }
+        rigidbody2d.velocity = new Vector2(xVel, rigidbody2d.velocity.y);
+    }
+
     private void GoBall()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
         if (Random.Range(0, 2) >= 0.5f)
         {
             rigidbody2d.AddForce(new Vector2(ballSpeed, 10));
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
 
     public GUISkin scoreSkin;
 
+    private Transform ball;
+
     public static void Score(string wallName)
     {
         if (wallName == "LeftWall")
@@ -21,11 +23,25 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball").transform;
+    }
+
     private void OnGUI()
     {
         GUI.skin = scoreSkin;
-        int symbolWidth = 64;
-        GUI.Label(new Rect(Screen.width / 2 - 150 - symbolWidth / 2, 10, symbolWidth, 100), PlayerScore1.ToString());
-        GUI.Label(new Rect(Screen.width / 2 + 150 - symbolWidth / 2, 10, symbolWidth, 100), PlayerScore2.ToString());
+        int symbolWidth = 32;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - symbolWidth / 2, 25, symbolWidth, 100), PlayerScore1.ToString());
+        GUI.Label(new Rect(Screen.width / 2 + 150 - symbolWidth / 2, 25, symbolWidth, 100), PlayerScore2.ToString());
+
+        int buttonWidth = 121;
+        int buttonHeight = 53;
+        if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth/2, 35, buttonWidth, buttonHeight), "RESET"))
+        {
+            PlayerScore1 = 0;
+            PlayerScore2 = 0;
+            ball.SendMessage(nameof(BallController.ResetBall)); // or GetComponent<BallController>() and call directly
+        }
     }
 }
